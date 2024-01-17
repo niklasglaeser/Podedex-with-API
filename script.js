561dac9941f92a574eb315b85bea36c1f508a865
niklas-glaeser <123083964+niklas-glaeser@users.noreply.github.com>
2024-01-17 22:01:16 +0100

    added bigCard and pokemon Infos design + logic

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-
 let allPokemon;
 let offset = 0;
 let limit = 30;
+let currentPokemonData;
+
 
 async function loadPokemon() {
     let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
@@ -68,7 +69,7 @@ async function openBigCard(pokemonId) {
     const bigCardContainer = document.getElementById('bigCardContainer');
     bigCardContainer.style.top = `${scrollY}px`;
 
-    document.body.classList.add('no-scroll');  // Disable scrolling
+    document.body.classList.add('no-scroll');
     document.getElementById('allPokemonContainer').classList.add('blur');
     bigCardContainer.style.display = 'flex';
 }
@@ -81,7 +82,6 @@ function closeBigCard() {
     const bigCardContainer = document.getElementById('bigCardContainer');
     bigCardContainer.style.display = 'none';
 
-    // Reset the top position
     bigCardContainer.style.top = '0';
 }
 
@@ -89,7 +89,12 @@ function closeBigCard() {
 async function renderBigCard(pokemonData, typesDivs) {
     const bigCardContainer = document.getElementById('bigCardContainer');
     bigCardContainer.innerHTML = pokemonCardTemplateBig(pokemonData, typesDivs);
+
+    await renderAbout(pokemonData);
+    
+    currentPokemonData = pokemonData;
 }
+
 
 function pokemonCardTemplate(pokemonData, typeDivs) {
     const pokemonNameCaps = pokemonData.name.toUpperCase();
@@ -110,17 +115,30 @@ function pokemonCardTemplateBig(pokemonData, typesDivs) {
 
     return `
         <div class="pokemonCardBig ${type}Background">
-            <div class="pokemonNameBig">${pokemonNameCaps}</div>
-            <div class="pokemonTypesBig">${typesDivs}</div>
+            <div class="bigCardButtons">
+                <button onClick="showPreviousPokemon(${pokemonData.id})"><img class="previousArrow"src="img/previous.png"></button>
+                <button onClick="closeBigCard()"><img class="closeButton"src="img/close.png"></button>
+                <button onClick="showNextPokemon(${pokemonData.id})"><img class="nextArrow"src="img/next.png"></button>
+            </div>
+            <div class="bigCardHeader">
+                <div class="bigCardNameTypes">
+                    <div class="pokemonNameBig">${pokemonNameCaps}</div>
+                    <div class="pokemonTypesBig">${typesDivs}</div>
+                </div>
+                <div class="pokemonIdBig">${pokemonData.id}</div>
+            </div>
             <img class="pokemonImageBig" src="${pokemonData.sprites.other['official-artwork'].front_default}">
-            <div class="pokemonIdBig">${pokemonData.id}</div>
-            <button onClick="showPreviousPokemon(${pokemonData.id})">Previous</button>
-            <button onClick="showNextPokemon(${pokemonData.id})">Next</button>
-            <button onClick="closeBigCard()">Close</button>
+            <div class="bigCardBottom">
+                <div class="bigCardMenu">
+                    <div onClick="renderAbout(currentPokemonData)">About</div>
+                    <div onClick="renderBaseStats(currentPokemonData)">Base Stats</div>
+                    <div onClick="renderMoves(currentPokemonData)">Moves</div>
+                </div>
+                <div id="bigCardBottomInfosContainer"></div>
+            </div>
         </div>
     `;
 }
-
 
 
 
@@ -158,3 +176,91 @@ async function updateBigCard(pokemonId) {
 
     renderBigCard(pokemonData, typesDivs);
 }
+
+function renderAbout(currentPokemonData) {
+    const bigCardBottomInfosContainer = document.getElementById('bigCardBottomInfosContainer');
+
+    const height = currentPokemonData.height;
+    const weight = currentPokemonData.weight;
+
+    let abilitiesString = '';
+    for (let i = 0; i < currentPokemonData.abilities.length; i++) {
+        abilitiesString += currentPokemonData.abilities[i].ability.name;
+        if (i < currentPokemonData.abilities.length - 1) {
+            abilitiesString += ', ';
+        }
+    }
+
+    const aboutContent = `
+        <div class="aboutInfoContainer">
+            <div class="aboutInfo">
+                <div class="aboutTitle">Height</div>
+                <div class="aboutValue">${height} dm</div>
+            </div>
+            <div class="aboutInfo">
+                <div class="aboutTitle">Weight</div>
+                <div class="aboutValue">${weight / 10} kg</div>
+            </div>
+            <div class="aboutInfo">
+                <div class="aboutTitle">Abilities</div>
+                <div class="aboutValue">${abilitiesString}</div>
+            </div>
+        </div>
+    `;
+
+    bigCardBottomInfosContainer.innerHTML = aboutContent;
+}
+
+
+async function renderBaseStats(currentPokemonData) {
+    const bigCardBottomInfosContainer = document.getElementById('bigCardBottomInfosContainer');
+
+    const stats = currentPokemonData.stats;
+
+    let statsContent = '';
+    for (let i = 0; i < stats.length; i++) {
+        const statName = stats[i].stat.name;
+        const baseStat = stats[i].base_stat;
+
+        statsContent += `
+            <div class="baseStatInfo">
+                <div class="baseStatTitle">${statName}</div>
+                <div class="baseStatValue">${baseStat}</div>
+            </div>
+        `;
+    }
+
+    const baseStatsContent = `
+        <div class="baseStatsContainer">
+            ${statsContent}
+        </div>
+    `;
+
+    bigCardBottomInfosContainer.innerHTML = baseStatsContent;
+}
+
+async function renderMoves(currentPokemonData) {
+    const bigCardBottomInfosContainer = document.getElementById('bigCardBottomInfosContainer');
+
+    const moves = currentPokemonData.moves;
+
+    let movesContent = '';
+    for (let i = 0; i < moves.length; i++) {
+        const moveName = moves[i].move.name;
+
+        movesContent += `
+            <div class="moveInfo">
+                <div class="moveName">${moveName}</div>
+            </div>
+        `;
+    }
+
+    const movesContainer = `
+        <div class="movesContainer">
+            ${movesContent}
+        </div>
+    `;
+
+    bigCardBottomInfosContainer.innerHTML = movesContainer;
+}
+
